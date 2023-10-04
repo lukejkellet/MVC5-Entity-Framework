811e03585a89b04375203cdc14a6e74d99a43e4c
Luke Kellet <lukejameskellet@hotmail.com>
2023-10-04 14:23:56 +0100

    Add project files.

---
+++ ProductsApp/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("product name")]
+        [StringLength(50)]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "Price is required")]
+        [Range(1, 1000, ErrorMessage = "Price must be between 1 and 1000")]
+        public decimal Price { get; set; }
+        [DisplayName("product description")]
+        [StringLength(200)]
+        public string Description { get; set; }
+    }
+}
